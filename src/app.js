67fd4f113285ce80fe84a5b03fbb90bc87194244
kabir02091999 <kabir.02091999@gmail.com>
2025-09-18 19:27:31 -0400

    cors y const

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import usuariosRoutes from './Routes/usuarios.routes.js';
 import inscripcionRoutes from './Routes/inscripcion.routes.js'; // Assuming you have inscripcionRoutes defined
 
 const app = express();
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());   
